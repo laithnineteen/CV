a61c6f949d2adc42ace6cce9185149c7fb91f81f
laithnineteen <72062230+laithnineteen@users.noreply.github.com>
2020-12-22 09:03:56 -0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,13 +5,13 @@ if (empt == "")
 alert("Please input your ID");
 return false;
 }
-     var empt1 = document.forms["form"]["URL"].value;
+     var empt1 = document.forms["form"]["url"].value;
 if (empt1 == "")
 {
 alert("Please input a URL");
 return false;
 }
-     var empt2 = document.forms["form"]["Message"].value;
+     var empt2 = document.forms["form"]["message"].value;
 if (empt2 == "")
 {
 alert("Please input a Message");
@@ -29,18 +29,11 @@ if (empt4 == "")
 alert("Please input your email");
 return false;
 }
+    if(isNaN(empt)){
+        alert("Please enter a valid ID only containing numbers");
+    }
+    var domain1=empt4.split("@");
+    alert(domain1[1]);
+    var user=empt1.split(".");
+    alert(user[0]);
 }
-
-function alphanumeric()
-{
- var letterNumber = /^[0-9a-zA-Z]+$/;
- if((inputtxt.value.match(letterNumber))
-  {
-   return true;
-  }
-else
-  { 
-   alert("message"); 
-   return false; 
-  }
-  }
